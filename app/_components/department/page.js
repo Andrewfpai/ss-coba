449f5ebf81a209adb229feadb3a4f1a312e5a6f5
Benedictus Leon Christian <bleonc24@gmail.com>
2024-08-03 19:20:29 +0700

    y

--- app/_components/department/page.js
+++ app/_components/department/page.js
@@ -152,10 +152,10 @@ const Department = (props) => {
                     return (
                       
                         <div key={index} className="department-service-box department-gradient 2l:w-[24.1875em] sm:w-[31%] 2xs:w-[48%] w-[70%] lg:h-[15.625em] h-[13em] rounded-[15px] font-bold text-[0.667em] tracking-[0.4px] sm:mx-0 mx-auto">
-                            <Link href={"/departemen-kami/"+content.href} className="flex flex-col items-center justify-center gap-[1.25em] w-full h-full relative z-[50]" passHref>
+                            <a href={"/departemen-kami/"+content.href} className="flex flex-col items-center justify-center gap-[1.25em] w-full h-full relative z-[50]">
                               <Image  width={0} height={0} sizes='100vw' className="w-[3.5em] relative z-[100]" src={content.icon} alt="" />
                               <h3 className="relative z-[100]">{content.name}</h3>
-                            </Link>
+                            </a>
                         </div>
                       
                     )
@@ -209,13 +209,13 @@ const Department = (props) => {
                     return (
               
                         <div key={index} className="department-service-box service-gradient relative 2l:w-[24.1875em] sm:w-[31%] 2xs:w-[48%] w-[70%] lg:h-[15.625em] h-[13em] rounded-[15px] font-bold text-[0.667em] tracking-[0.4px] sm:mx-0 mx-auto">
-                            {/* <Link key={index} href={"/layanan-kami/"+content.href} className="w-full h-full flex flex-col items-center justify-center gap-[1.25em]" passHref> */}
+                            <a key={index} href={"/layanan-kami/"+content.href} className="w-full h-full flex flex-col items-center justify-center gap-[1.25em]">
                               <Image width={0} height={0} sizes='100vw' className="w-[3.5em]" src={content?.icon} alt="" />
                               <h3 className="">{content?.name}</h3>
                               <div className={content?.isPromo?'promo absolute top-[0.75em] left-[0.8em] bg-[#FF0000] w-[4.625em] h-[1.875em] rounded-[1.25em] pt-[0.3em] text-white':'hidden'}>
                                   <div className=" font-black text-center text-[0.875em]">PROMO</div>
                               </div>
-                            {/* </Link> */}
+                            </a>
                         </div>
                  
                     )
